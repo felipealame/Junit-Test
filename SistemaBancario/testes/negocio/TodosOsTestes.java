cb18fbd84c83ccdb00861d61fe7341d7da3053ab
Felipe Alame <felipe.alame@gmail.com>
2021-09-27 17:23:23 -0300

    Projeto de JUnit para testes simples

---
+++ SistemaBancario/testes/negocio/TodosOsTestes.java
@@ -0,0 +1,15 @@
+package negocio;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ GerenciadoraClientesTest_Ex10.class,
+	GerenciadoraContasTest_Ex11.class })
+public class TodosOsTestes {
+
+}
+
+ 
+
